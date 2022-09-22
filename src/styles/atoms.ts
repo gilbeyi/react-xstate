38220a62aac6861364c9cccfdd5f8580d3144ec0
r.i <gilbeyi.w23@gmail.com>
2022-09-23 02:45:57 +0900

    feat:create style files directory and file

---
+++ src/styles/atoms.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react'
+
+export const input = css({
+  marginBottom: '10px',
+  padding: '8px',
+  fontSize: '14px',
+  border: '1px solid #CCC',
+  borderRadius: '5px',
+  outline: 'none'
+})
