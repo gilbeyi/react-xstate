6cacdacf088ad2e559a3df44638d16e91c95bc4e
r.i <gilbeyi.w23@gmail.com>
2022-09-24 20:07:20 +0900

    style:change text style

--- src/styles/atoms.ts
+++ src/styles/atoms.ts
@@ -1,10 +1,12 @@
 import { css } from '@emotion/react'
 
 export const text = css({
+  width: '100%',
   marginBottom: '10px',
   padding: '8px',
   fontSize: '14px',
   border: '1px solid #CCC',
   borderRadius: '5px',
-  outline: 'none'
+  outline: 'none',
+  boxSizing: 'border-box'
 })
